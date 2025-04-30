b7c1716f82965aff7e7402104dadbb5bf561b5f1
alexp230 <apruitt230@gmail.com>
2025-04-30 16:32:04 -0500

    Added navbar

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -5,17 +5,23 @@ import './App.css'
 import MovieCard from "./Components/MovieCard"
 import Home from "./Pages/Home"
 import Favorites from "./Pages/Favorites"
+import NavBar from "./Components/NavBar"
 
 function App()
 {
 
   return(
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/favorites" element={<Favorites />}/>
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/favorites" element={<Favorites />}/>
+        </Routes>
+      </main>
+      
+    </div>
     
   )
 }

2c04fb25fb9ee25e73861407c8e7f302178d194d
alexp230 <apruitt230@gmail.com>
2025-04-30 16:17:38 -0500

    Added routes

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,20 +1,23 @@
+import {Routes, Route} from "react-router-dom"
+
 import './App.css'
+
 import MovieCard from "./Components/MovieCard"
 import Home from "./Pages/Home"
+import Favorites from "./Pages/Favorites"
 
 function App()
 {
 
   return(
-    <>
-      <Home />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/favorites" element={<Favorites />}/>
+      </Routes>
+    </main>
+    
   )
-}
-
-function Text()
-{
-  
 }
 
 
